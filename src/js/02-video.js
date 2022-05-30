151da8f2d61b40665fd98c9491672ba0fa9dca3a
Slava Grobovoy <slava.grobovoy27@ukr.net>
2022-05-30 10:31:54 +0300

    mentor's edits

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,8 +6,18 @@ const player = new Vimeo.Player(iframe);
 const setCurrentTime = localStorage.getItem("videoplayer-current-time")
 
 player.setCurrentTime(setCurrentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-})
+    
+}).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 
 
